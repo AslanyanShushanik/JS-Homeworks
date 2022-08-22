c4b0d09e5e3b4d5360f51747dc4ce16263975def
AslanyanShushanik <49529320+AslanyanShushanik@users.noreply.github.com>
2022-08-22 10:56:14 +0400

    Update array1DIterate.js

--- array1DIterate.js
+++ array1DIterate.js
@@ -1,30 +1,32 @@
-// this feels unnecessary
-
-function array1DIterateOP1(array) {
-  for (let i = 0; i < 1; i++) {
-    for (let j = 0; j < array.length; j++) {
-      console.log(array[j]);
+function array1DIterateOP2(array, elem) {
+  let indx = -1;
+  for (let i = 0; i < array.length; i += 2) {
+    if (elem === array[i]) {
+      indx = i;
     }
-  }
-}
-
-let array = [0, 1, 2, 3, 4, 5];
-
-array1DIterateOP1(array);
-
-// this feels terrible
-
-function array1DIterateOP2(array) {
-  for (let i = 0; i < array.length; i++) {
-    if (i % 2 === 0) {
-      console.log(array[i]);
-    }
-    for (let j = 0; j < array.length; j++) {
-      if (j % 2 !== 0 && j === i + 1) {
-        console.log(array[j]);
+    for (let j = i + 1; j <= i + 1; j++) {
+      if (j % 2 !== 0 && elem === array[j]) {
+        indx = j;
       }
     }
   }
+  return indx;
 }
 
-array1DIterateOP2(array);
+const array = [0, 1, 2, 3, 4, 5];s
+console.log(array1DIterateOP2(array, 4));
+
+
+// a bit of workaround :D
+
+function array1DIterateOP1(array, elem) {
+  let indx = -1;
+  for (let i = 0; i < 1; i++) {
+    for (let j = 0; j < array.length; j++) {
+      if (array[j] === elem) indx = j;
+    }
+  }
+  return indx;
+}
+
+console.log(array1DIterateOP1(array, 3));
